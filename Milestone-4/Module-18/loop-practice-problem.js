ef07e8522a4c073fbf8f0f365953131b9c31a35f
mdmahbubekhuda <md.mahbub.e@gmail.com>
2023-08-02 12:25:48 +0600

    module-19 begins

---
+++ Milestone-4/Module-18/loop-practice-problem.js
@@ -0,0 +1,67 @@
+// 39 times while loop
+// let myMood = 1;
+// while(myMood<=39){
+//     console.log("I'm awsome");
+//     myMood++
+// }
+
+// number between 58 to 98
+// for (let i = 58; i <= 98; i++) {
+//     console.log(i);
+// }
+
+// all even numbers from 412 to 456
+// for (let i = 412; i <= 456; i += 2) {
+//     console.log(i);
+// }
+
+// all odd numbers between 581 and 623
+// let oddNumbers = 581;
+// while (oddNumbers <= 623) {
+//     console.log(oddNumbers);
+//     oddNumbers += 2;
+// }
+
+// array display elements using for loop
+// let programmingLanguages = ['HTML5', 'CSS3', 'JavaScript'];
+// for (let i = 0; i < programmingLanguages.length; i++) {
+//     console.log(programmingLanguages[i]);
+// }
+
+// array display elements using while loop
+// let cellPhoneBrandsUsed = ['Simens', 'Nokia', 'SonyEricson', 'Xiaomi'];
+// let i = 0;
+// while (i < cellPhoneBrandsUsed.length) {
+//     console.log(cellPhoneBrandsUsed[i]);
+//     i++;
+// }
+
+// loop breaks at 40 
+// for (let i = 30; i <= 86; i++) {
+//     if (i == 40) {
+//         break;
+//     }
+//     else {
+//         console.log('Keep rolling');
+//     }
+// }
+
+// loop continue over 200
+// let priceOfBooks = [100, 200, 250, 120, 400, 300, 150];
+// for (i = 0; i < priceOfBooks.length; i++) {
+//     if (priceOfBooks[i] > 200) {
+//         continue;
+//     }
+//     else {
+//         console.log(priceOfBooks[i]);
+//     }
+// }
+
+// display numbers between 1 to 100 only which are devisible by 3
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+}
+
+
